47aad8d73adc5525e3d1b5a92160b15651fe7ff9
skaanboldan <147402161+skaanboldan@users.noreply.github.com>
2024-01-09 14:17:38 +0300

    bug fix

--- zabbixReportConsumer/src/main/java/com/consumer/zabbix/report/zabbixReportConsumer/MailController/GmailSender.java
+++ zabbixReportConsumer/src/main/java/com/consumer/zabbix/report/zabbixReportConsumer/MailController/GmailSender.java
@@ -11,7 +11,7 @@ public class GmailSender {
     public static void sendEmail(String recipientEmail, String subject, String content, String attachmentPath, String s) {
         String senderEmail = "kaanboldan@gmail.com";
         String senderPassword = "zpka mtdh zhqu exwx"; // Replace with your actual Gmail password
-
+System.out.println("test1");
         // Mail server settings
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
@@ -26,11 +26,15 @@ public class GmailSender {
                 return new PasswordAuthentication(senderEmail, senderPassword);
             }
         };
+        System.out.println("test2");
 
         // Create a session
         Session session = Session.getInstance(props, authenticator);
+        System.out.println("test3");
 
         try {
+            System.out.println("test4");
+
             // Create a MimeMessage
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(senderEmail));
@@ -43,6 +47,7 @@ public class GmailSender {
             // Create text part
             MimeBodyPart textPart = new MimeBodyPart();
             textPart.setText(content);
+            System.out.println("test5");
 
             // Attach the text part
             multipart.addBodyPart(textPart);
