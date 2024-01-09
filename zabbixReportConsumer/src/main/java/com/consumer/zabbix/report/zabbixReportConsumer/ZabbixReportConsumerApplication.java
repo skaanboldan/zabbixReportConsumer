f4b33170df0b0a4845896739f781555835510f76
skaanboldan <147402161+skaanboldan@users.noreply.github.com>
2024-01-09 15:08:07 +0300

    bug fix

--- zabbixReportConsumer/src/main/java/com/consumer/zabbix/report/zabbixReportConsumer/ZabbixReportConsumerApplication.java
+++ zabbixReportConsumer/src/main/java/com/consumer/zabbix/report/zabbixReportConsumer/ZabbixReportConsumerApplication.java
@@ -3,11 +3,27 @@ package com.consumer.zabbix.report.zabbixReportConsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+
 @SpringBootApplication
 public class ZabbixReportConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZabbixReportConsumerApplication.class, args);
+		try {
+			String  address = InetAddress.getByName("www.google.com").getHostAddress();
+			InetAddress inet = InetAddress.getByName(address);
+			System.out.println("Sending Ping Request to " + address);
+			if(inet.isReachable(50000)){
+				System.out.println("Host is reachable");
+			}
+			else{
+				System.out.println("Host is not reachable");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
