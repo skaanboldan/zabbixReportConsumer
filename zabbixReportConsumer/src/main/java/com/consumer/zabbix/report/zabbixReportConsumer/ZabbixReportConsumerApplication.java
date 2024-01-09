dced86e3cfd2faeeed5cd8c1241ed21a3a3993e0
skaanboldan <147402161+skaanboldan@users.noreply.github.com>
2024-01-09 15:18:19 +0300

    bug fix

--- zabbixReportConsumer/src/main/java/com/consumer/zabbix/report/zabbixReportConsumer/ZabbixReportConsumerApplication.java
+++ zabbixReportConsumer/src/main/java/com/consumer/zabbix/report/zabbixReportConsumer/ZabbixReportConsumerApplication.java
@@ -10,6 +10,7 @@ import java.net.InetAddress;
 public class ZabbixReportConsumerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test başlıyo...");
 		try {
 			String  address = InetAddress.getByName("www.google.com").getHostAddress();
 			InetAddress inet = InetAddress.getByName(address);
